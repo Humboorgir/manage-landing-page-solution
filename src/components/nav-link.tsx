24d0b5c4eca7d0571d74dc563c3a06777638e5ab
Humboorgir <hardcraftap@gmail.com>
2024-09-02 18:10:41 +0330

    feat: implement opening animation for mobile nav

---
+++ src/components/nav-link.tsx
@@ -0,0 +1,32 @@
+import { twMerge } from "tailwind-merge";
+
+// comes with a simple and interesting hover effect
+type NavLinkProps = {
+  link: {
+    href: string;
+    label: string;
+  };
+  className?: string;
+};
+
+const NavLink = ({ link, className }: NavLinkProps) => {
+  return (
+    <a
+      className={twMerge(
+        "py-2.5 md:py-0.5 shrink-0 group overflow-hidden relative font-bold text-sm text-slate-600/90",
+        className
+      )}
+      href={link.href}
+    >
+      <span className="text-transparent">{link.label}</span>
+      <div className="duration-[250ms] group-hover:-translate-y-full transition-all absolute left-0 right-0 top-0 bottom-0">
+        {link.label}
+      </div>
+      <div className="duration-[250ms] text-primary translate-y-full group-hover:translate-y-0 transition-all absolute left-0 right-0 top-0 bottom-0">
+        {link.label}
+      </div>
+    </a>
+  );
+};
+
+export default NavLink;
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Button from "./button";
+import NavLink from "./nav-link";
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -20,52 +21,40 @@ const Navbar = () => {
       className="relative flex items-center w-full justify-between mb-16 md:mb-28
     md:grid md:grid-cols-[auto,1fr,auto] md:justify-between"
     >
+      {/* backdrop  */}
       <div
         data-show={menuOpen}
         onClick={toggleMenu}
-        className="hidden data-[show=true]:block fixed left-0 right-0 top-0 bottom-0 bg-gradient-to-b from-black/10 to-black/50"
+        className="invisible data-[show=true]:visible block fixed left-0 right-0 top-0 bottom-0 bg-gradient-to-b from-black/10 to-black/50
+        opacity-0 data-[show=true]:opacity-100 transition-opacity duration-300"
       />
+      {/* mobile navigation */}
       <div
         data-show={menuOpen}
-        className="hidden data-[show=true]:flex absolute flex-col items-center
+        className="delay-100 invisible opacity-0 -translate-y-4 data-[show=true]:opacity-100 data-[show=true]:translate-y-0
+        transition-[opacity,transform] duration-300 data-[show=true]:visible flex absolute flex-col items-center
         p-8 top-[calc(100%+32px)] bg-white left-0 right-0"
       >
-        {links.map((link) => {
-          return (
-            <a href={link.href} className="font-bold text-slate-600 py-2.5">
-              {link.label}
-            </a>
-          );
+        {links.map((link, i) => {
+          return <NavLink data-show={menuOpen} key={i} link={link} />;
         })}
       </div>
+
       <img src="/logo.svg" alt="Logo" />
       <div className="hidden md:flex justify-center items-center w-full space-x-4">
         {links.map((link) => {
-          return (
-            <a
-              className="shrink-0 group overflow-hidden relative font-bold text-sm text-slate-600/90"
-              href={link.href}
-            >
-              <span className="text-transparent">{link.label}</span>
-              <div className="duration-[250ms] group-hover:-translate-y-full transition-all absolute left-0 right-0 top-0 bottom-0">
-                {link.label}
-              </div>
-              <div className="duration-[250ms] text-orange-600 translate-y-full group-hover:translate-y-0 transition-all absolute left-0 right-0 top-0 bottom-0">
-                {link.label}
-              </div>
-            </a>
-          );
+          return <NavLink link={link} />;
         })}
       </div>
       <Button className="hidden md:block w-fit ml-auto">Get started</Button>
 
       {/* hamburger icon  */}
       <img
-        aria-label="open menu"
+        aria-label={menuOpen ? "close menu" : "open menu"}
         role="button"
         onClick={toggleMenu}
-        className="md:hidden"
-        src="/icon-hamburger.svg"
+        className="md:hidden z-10 hover:bg-white p-2 rounded-md transition-colors"
+        src={menuOpen ? "/icon-close.svg" : "/icon-hamburger.svg"}
         alt="Hamburger icon"
       />
     </div>
